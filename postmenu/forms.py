207d560a5756d8854ca0475a694ebab058cf735e
RafaelAmbrozius <dmitriy-zyubin@mail.ru>
2025-01-27 14:13:15 +0300

    new cimm

--- postmenu/forms.py
+++ postmenu/forms.py
@@ -3,5 +3,5 @@ from .models import Post
 
 
 class PostForm(forms.Form):
-    title = forms.CharField(max_length=50, label='Заголовок поста')
-    text = forms.CharField(widget=forms.Textarea(), label='Текст поста')
+    title = forms.CharField(max_length=50, label='Назвыание книги')
+    text = forms.CharField(widget=forms.Textarea(), label='Описание книги')
